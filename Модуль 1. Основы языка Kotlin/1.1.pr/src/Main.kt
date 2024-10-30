1aaaf73cb6a5215401c3dbb2a91d9d9e3f073683
Hohrandrey <EnderBro_2005@mail.ru>
2024-10-30 22:59:52 +0300

    1.1

---
+++ Модуль 1. Основы языка Kotlin/1.1.pr/src/Main.kt
@@ -0,0 +1,37 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+
+    //Задание 1
+    /*
+    val (a, b, m, n) = readln().split(" ").map { it.toDouble() }
+    if ((a >=m && b>=n) or (a >= n && b>=m)){
+        println("YES")
+    }
+    else
+        println("NO")
+
+     */
+
+    //Задание 2
+    val nukl = readln()
+
+    var count_A = 0
+    var count_T = 0
+    var count_G = 0
+    var count_C = 0
+
+    for (i in nukl) {
+        if (i == 'A')
+            count_A += 1
+        else if (i == 'T')
+            count_T += 1
+        else if (i == 'G')
+            count_G += 1
+        else
+            count_C += 1
+    }
+
+    println("$count_A $count_T $count_G $count_C")
+}
+
